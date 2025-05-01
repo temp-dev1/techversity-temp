800724853c15d4ee9c233bf05dc743e3edad5b47
temp-dev1 <temp0xd@proton.me>
2025-05-01 18:17:33 +0530

    Update courses.ts

--- data/courses.ts
+++ data/courses.ts
@@ -1,6 +1,23 @@
 import { connectDB } from "../lib/db";
 import Course from "../models/Course";
 
+
+export interface Course {
+  _id: string;  // MongoDB default _id
+  id: number;
+  title: string;
+  image: string;
+  rating: number;
+  reviews: number;
+  duration: string;
+  level: string;
+  price: number;
+  discountedPrice: number;
+  nextBatch: string;
+  category: string;
+  features: string[];
+}
+
 // Immediately-invoked async function to fetch and export courses
 let allCourses: any[] = [];
 
