a756c277476ab5747211054aecc78a8435e6c87d
temp-dev1 <temp0xd@proton.me>
2025-05-01 12:12:04 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,23 @@
-import AboutUs from "@/components/about/AboutUs";
+import AboutHero from "@/components/about/AboutHero";
+import AboutStats from "@/components/about/AboutStats";
+import AboutContent from "@/components/about/AboutContent";
+import AboutFeatures from "@/components/about/AboutFeatures";
+import AboutCertifications from "@/components/about/AboutCertifications";
+import AboutTeam from "@/components/about/AboutTeam";
+import AboutTestimonials from "@/components/about/AboutTestimonials";
+import AboutContact from "@/components/about/AboutContact";
 
 export default function AboutPage() {
-  return <AboutUs />;
+  return (
+    <main className="min-h-screen pt-24">
+      <AboutHero />
+      <AboutStats />
+      <AboutContent />
+      <AboutFeatures />
+      <AboutCertifications />
+      <AboutTeam />
+      <AboutTestimonials />
+      <AboutContact />
+    </main>
+  );
 }
