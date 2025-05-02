4325132a14c55a60a407a6e96d160325340c241d
temp-dev1 <temp0xd@proton.me>
2025-05-02 15:00:03 +0530

    Update Course.ts

--- models/Course.ts
+++ models/Course.ts
@@ -1,16 +1,34 @@
 import mongoose from 'mongoose';
 
 const CourseSchema = new mongoose.Schema({
+  id: Number,
   title: String,
-  category: String,
   image: String,
-  duration: String,
-  level: String,
   rating: Number,
   reviews: Number,
+  duration: String,
+  level: String,
   price: Number,
   discountedPrice: Number,
+  nextBatch: String,
+  category: String,
   features: [String],
 });
+
+export interface Course {
+  _id: string;
+  id: number;
+  title: string;
+  image: string;
+  rating: number;
+  reviews: number;
+  duration: string;
+  level: string;
+  price: number;
+  discountedPrice: number;
+  nextBatch: string;
+  category: string;
+  features: string[];
+}
 
 export default mongoose.models.Course || mongoose.model('Course', CourseSchema);
