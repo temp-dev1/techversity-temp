7666b90095ffab03d2969a4598abe035192f090e
temp-dev1 <temp0xd@proton.me>
2025-05-02 15:48:10 +0530

    Create CertPartner.ts

---
+++ models/CertPartner.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const CertPartnerSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  logo: String,
+});
+
+export interface CertPartner {
+  _id: string;
+  id: number;
+  name: string;
+  logo: string;
+}
+
+export default mongoose.models.CertPartner || mongoose.model('CertPartner', CertPartnerSchema);
